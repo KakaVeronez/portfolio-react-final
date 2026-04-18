c9e53d3935c23c02f01f29cba8f4be42a418f7c6
Kaká Veronez <147287110+KakaVeronez@users.noreply.github.com>
2026-04-18 00:57:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Header from './components/Header';
+import SecaoProjetos from './components/SecaoProjetos';
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <SecaoProjetos />
+    </div>
+  );
+}
+
+export default App;
