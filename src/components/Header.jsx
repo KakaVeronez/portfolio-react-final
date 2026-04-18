572e74c3ae567b806f2634595bfcc4386c0e1ddd
Kaká Veronez <147287110+KakaVeronez@users.noreply.github.com>
2026-04-18 00:50:35 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,7 @@
-
+export default function Header() {
+  return (
+    <header style={{ padding: '20px', background: '#333', color: 'white' }}>
+      <h1>Carlos | Designer & Developer</h1>
+    </header>
+  );
+}
