36453127ea3c872dd794d618b0a7bb1c032a7a4f
Kaká Veronez <147287110+KakaVeronez@users.noreply.github.com>
2026-04-18 00:51:35 -0300

    Update projetos.js

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -1,1 +1,20 @@
-
+export const meusProjetos = [
+  {
+    id: 1,
+    titulo: "PodoClin",
+    descricao: "Plataforma de gestão para podologia com foco em UX.",
+    link: "#"
+  },
+  {
+    id: 2,
+    titulo: "EcoRit",
+    descricao: "Branding e design de embalagem sustentável para açaí.",
+    link: "#"
+  },
+  {
+    id: 3,
+    titulo: "Móveis Multifuncionais",
+    descricao: "Design de mobiliário inteligente para espaços reduzidos.",
+    link: "#"
+  }
+];
